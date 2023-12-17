045a8edf0f971fa266907ef5d6cfbc586ae811f4
Vivek Neeralagi <139262048+VivekNeer@users.noreply.github.com>
2023-12-17 11:55:50 +0530

    Stylized the slider

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function to handle box click events
 function handleBoxClick(box, useRandomColor) {
     if (useRandomColor) {
         // Change the color to a random color on click
@@ -35,6 +36,13 @@ window.addEventListener("load", function () {
         // Toggle the flag when the random button is clicked
         useRandomColor = !useRandomColor;
     });
+
+    // Event listener for the default button
+    const defaultButton = document.getElementById('defaultButton');
+    defaultButton.addEventListener('click', function () {
+        // Set the flag to false to use the default behavior
+        useRandomColor = false;
+    });
 });
 
 // Function to reset box colors to default
@@ -59,3 +67,23 @@ function getRandomColor() {
 // Event listener for the reset button
 const resetButton = document.getElementById('resetButton');
 resetButton.addEventListener('click', resetBoxes);
+
+
+// kudos to https://codepen.io/armanb/details/GRBjVgd 
+
+const slider_input = document.getElementById('slider_input'),
+      slider_thumb = document.getElementById('slider_thumb'),
+      slider_line = document.getElementById('slider_line');
+
+function showSliderValue() {
+  slider_thumb.innerHTML = slider_input.value;
+  const bulletPosition = (slider_input.value /slider_input.max),
+        space = slider_input.offsetWidth - slider_thumb.offsetWidth;
+
+  slider_thumb.style.left = (bulletPosition * space) + 'px';
+  slider_line.style.width = slider_input.value + '%';
+}
+
+showSliderValue();
+window.addEventListener("resize",showSliderValue);
+slider_input.addEventListener('input', showSliderValue, false);
